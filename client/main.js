855aa23fce28f05e1b04100d534c0c1e15f96a71
Sergey <serhiyborsuk@gmail.com>
2017-08-07 01:21:51 +0300

    calculate players position

--- client/main.js
+++ client/main.js
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from 'meteor/tracker';
 
-import { Players } from '../imports/api/players.js';
+import { Players, calculatePlayerPositions } from '../imports/api/players.js';
 import { App } from '../imports/components/App';
 
 Meteor.startup(() => {
     Tracker.autorun(() => {
-        let players = Players.find({}, {
-            sort: {
-                score: -1
-            }
-        }).fetch();
+        let players = Players.find({}, {sort: {score: -1}}).fetch();
+        let positionedPlayers = calculatePlayerPositions(players);
 
-        ReactDOM.render(<App title="Score Keeper" players={players} />, document.getElementById('container'));
+
+        ReactDOM.render(<App title="Score Keeper" players={positionedPlayers} />, document.getElementById('container'));
     });
 });
 
--- imports/components/Player.js
+++ imports/components/Player.js
@@ -10,7 +10,7 @@ export class Player extends React.Component
             <div key={this.props.player._id} className="item">
                 <div className="player">
                     <h3 className="player__name">{this.props.player.name}</h3>
-                    <p className="player__stats">{this.props.player.score} points(s)</p>
+                    <p className="player__stats">{this.props.player.rank} { this.props.player.positin} {this.props.player.score} points(s)</p>
                     <div className="player__actions">
                         <button className="button button--round" onClick={() => Players.update({_id: this.props.player._id }, {$inc: {score: 1}})}>+1</button>
                         <button className="button button--round" onClick={() => Players.update({_id: this.props.player._id }, {$inc: {score: -1}})}>-1</button>
